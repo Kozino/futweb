a0f804f1f7d4486dbf6656f6b06f56269981fa59
Nwankwo Henry Chiemerie <49980451+Kozino@users.noreply.github.com>
2026-09-03 14:44:46 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-```tsx
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -38,4 +38,4 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
-```
+
