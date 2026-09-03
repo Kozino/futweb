827acb6b8d43a1f82103d7e0c68ca109acca929f
Nwankwo Henry Chiemerie <49980451+Kozino@users.noreply.github.com>
2026-09-04 02:48:34 +0300

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,6 @@
 export const corsHeaders: Record<string, string> = {
   'Access-Control-Allow-Origin': Deno.env.get('ALLOWED_ORIGIN') ?? '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-futweb-signature',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-futweb-signature, x-application-name',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Max-Age': '86400',
 }
